911ed32fe07772229322965ab08cbba4dedfedbb
TwiceNo <ri.twice.no@gmail.com>
2021-03-01 00:02:40 +0300

    if-else

---
+++ 1/3.rb
@@ -0,0 +1,9 @@
+puts "What's your favorite programming language?"
+lang = gets.chomp
+if lang == "Ruby" or lang == "ruby"
+	puts "Don't be so flunky :)"
+elsif lang == "Python" or lang == "python"
+	puts "Great choice, I like it too"
+else 
+	puts "Soon it'll change to Ruby"
+end

655f218ce872b785769655cd5e3d7aae431326d9
TwiceNo <ri.twice.no@gmail.com>
2021-03-01 00:49:42 +0300

    sum, multiplication, minimum, maximum methods

--- 1/5-8.rb
+++ 1/5-8.rb
@@ -1,7 +1,47 @@
-num = ARGV[0].to_i
-sum = 0
-while num > 0 do
-	sum += num % 10
-	num /= 10
+def sum(num)
+	sum = 0
+	while num > 0 do
+		sum += num % 10
+		num /= 10
+	end
+	puts "Sum = #{sum}"
 end
-puts "Sum = #{sum}"
+
+def multi(num)
+	mul = 1
+	while num > 0 do
+		mul *= num % 10
+		num /= 10
+	end
+	puts "Multiplication = #{mul}"
+end
+
+def minimum(num)
+	min = num.clone
+	while num > 0 do
+		digit = num % 10
+		if digit < min 
+			min = digit.clone
+		end
+		num /=10
+	end
+	puts "Minimal = #{min}"
+end
+
+def maximum(num)
+	max = 0
+	while num > 0 do
+		digit = num % 10
+		if digit > max 
+			max = digit.clone
+		end
+		num /=10
+	end
+	puts "Maximal = #{max}"
+end
+
+x = ARGV[0].to_i
+sum(x.clone)
+multi(x.clone)
+minimum(x.clone)
+maximum(x.clone)

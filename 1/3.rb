fcd9059d27a58d28e8204ba5388efc9e823bf306
TwiceNo <ri.twice.no@gmail.com>
2021-03-01 00:05:28 +0300

    unless

--- 1/3.rb
+++ 1/3.rb
@@ -1,9 +1,7 @@
 puts "What's your favorite programming language?"
 lang = gets.chomp
-if lang == "Ruby" or lang == "ruby"
-	puts "Don't be so flunky :)"
-elsif lang == "Python" or lang == "python"
-	puts "Great choice, I like it too"
-else 
+unless lang == "Ruby" or lang == "ruby"
 	puts "Soon it'll change to Ruby"
+else 
+	puts "Don't be so flunky :)"
 end

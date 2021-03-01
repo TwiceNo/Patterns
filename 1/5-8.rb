e11f8f070a9130c881c2ee1935696b9238ee10d9
TwiceNo <ri.twice.no@gmail.com>
2021-03-01 08:38:51 +0300

    amount of even not coprime numbers

--- 1/5-8.rb
+++ 1/5-8.rb
@@ -40,8 +40,34 @@ def maximum(num)
 	puts "Maximal = #{max}"
 end
 
+
+def not_coprime(a, b)
+	return gcd(a, b) == 1
+end
+
+def gcd(a, b)
+	if b == 0
+		return a
+	else
+		gcd(b, a.clone % b)
+	end
+end
+
+def num_of_not_coprimes(num)
+	count = 0
+	for i in 2..num do
+		if i % 2 == 0 and not_coprime(num, i)
+			count += 1
+		end
+	end
+	puts "Num of even not coprimes = #{count}"
+end
+
+
+
 x = ARGV[0].to_i
 sum(x.clone)
 multi(x.clone)
 minimum(x.clone)
 maximum(x.clone)
+num_of_not_coprimes(x.clone)

7dc5fd2352dbc3ffaed0a290acdf4fa7e9de95c5
TwiceNo <ri.twice.no@gmail.com>
2021-03-18 19:23:17 +0300

    Найти количество четных чисел, не взаимно простых с данным

--- 1/5-8.rb
+++ 1/5-8.rb
@@ -7,6 +7,7 @@ def sum(num)
 	puts "Sum = #{sum}"
 end
 
+
 def multi(num)
 	mul = 1
 	while num > 0 do
@@ -15,6 +16,7 @@ def multi(num)
 	end
 	puts "Multiplication = #{mul}"
 end
+
 
 def minimum(num)
 	min = num.clone
@@ -27,6 +29,7 @@ def minimum(num)
 	end
 	puts "Minimal = #{min}"
 end
+
 
 def maximum(num)
 	max = 0
@@ -41,86 +44,23 @@ def maximum(num)
 end
 
 
-def not_coprime(a, b)
-	return gcd(a, b) != 1
-end
-
-def gcd(a, b)
-	if b == 0
-		return a
-	else
-		gcd(b, a.clone % b)
-	end
-end
-
-def num_of_not_coprimes(num)
+def not_coprime(num)
 	count = 0
-	for i in 2..num do
-		if i % 2 == 0 and not_coprime(num, i)
+	for i in (2..num - 1).step(2) do
+		if (num.gcd i) != 1
 			count += 1
 		end
 	end
-	puts "Num of even not coprimes = #{count}"
+	return count
 end
 
 
-def max_aliquant(num)
-	max = 0
-	while num > 0 do
-		digit = num % 10
-		if digit % 3 != 0 and digit > max 
-			max = digit.clone
-		end
-		num /=10
-	end
-	puts "Maximal aliquant 3 = #{max}"
-end
 
 
-def sum_lesser(num)
-	sum = 0
-	while num > 0 do
-		digit = num % 10
-		if digit < 5
-			sum += num % 10
-			num /= 10
-		end
-	end
-	return sum
-end
-
-def max_not_coprime(num)
-	ld = least_divisor(num.clone)
-	i = num.clone - 1
-	while i > 0
-		if not_coprime(num, i) and i % ld != 0 
-			puts i
-			return i
-		end
-		i -= 1
-	end
-end
-
-def least_divisor(num)
-	i = 2
-	while i < num / 2
-		if num % i == 0
-			puts i
-			return i
-		end
-		i += 1
-	end
-end
-
-def method_3(num)
-	puts "Method 3 = #{max_not_coprime(num.clone) * sum_lesser(num.clone)}"
-end
 
 x = ARGV[0].to_i
-sum(x.clone)
-multi(x.clone)
-minimum(x.clone)
-maximum(x.clone)
-num_of_not_coprimes(x.clone)
-max_aliquant(x.clone)
-method_3(x.clone)
+# sum(x.clone)
+# multi(x.clone)
+# minimum(x.clone)
+# maximum(x.clone)
+puts not_coprime(x)

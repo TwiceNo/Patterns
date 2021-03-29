50394ed6cdb60fa0f40270337749bd0397c2df29
TwiceNo <ri.twice.no@gmail.com>
2021-03-29 07:02:30 +0300

    task 15 complete

--- 2/12.rb
+++ 2/12.rb
@@ -71,15 +71,68 @@ def after_nums(line)
 	line.split(/[0-9]+/).map { |part| part[0] == " " ? part.split(" ").count : 0 }.max
 end
 
+def get_vowels()
+	%w[a e i o u]
+end
+
+def get_consonants(vowels)
+	("a".."z").to_a.reject { |e| vowels.include? e }
+end
+
+def get_frequency()
+	from_file("frequency.txt").map { |line|
+		line.split(" ") }.to_h.transform_values { |v| v.to_f }
+end
 
 
-vowels = %w[a e i o u]
-consonants = ("a".."z").to_a.reject { |e| vowels.include? e }
-frequency = from_file("frequency.txt").map { |line| 
-	line.split(" ") }.to_h.transform_values { |v| v.to_f }
+#Control
+
+def process(param, lines)
+	puts "\nTask #{param}\n"
+	puts show(param)
+	case param
+	when 12
+		puts by_length(lines)
+	when 13
+		puts by_words(lines)
+	when 14
+		puts by_after_nums(lines)
+	when 1
+		puts by_vowels_consonants(lines, get_vowels(), get_consonants(get_vowels))
+	when 5
+		puts by_frequent(lines, get_frequency())
+	when 9
+		puts by_ascii_code(lines)
+	when 10
+		puts by_triads(lines)
+	end
+end
+
+def show(param)
+	case param
+	when 12
+		"By length:"
+	when 13
+		"By words:"
+	when 14
+		"By after nums:"
+	when 1
+		"By vowels-consonants:"
+	when 5
+		"By frequent char:"
+	when 9
+		"By ASCII code:"
+	when 10
+		"By mirrored triads:"
+	else
+		"Unknown task"
+	end
+end
+
 
 lines = from_file("text2.txt")
-
 puts lines
-puts
-puts by_after_nums(lines)
+
+for el in ARGV do
+	process(el.to_i, line)
+end

f80857bda089f7486be7c8f46bf23f59abc5ea83
TwiceNo <ri.twice.no@gmail.com>
2021-03-29 08:32:04 +0300

    task 11 -- 4

--- 2/10-11.rb
+++ 2/10-11.rb
@@ -1,8 +1,14 @@
+#4 11 15
+
 def dates(line)
   line.scan(/([0-9]|[1-2][0-9]|[3][0-1]) (January|February|March|April|May|June|July|August|September|October|November|December) ([1-2][0-9]{3})/)
+end
+
+def lesser_than(line)
+  line.scan(/[0-4]/).count
 end
 
 puts "Enter line:"
 line = gets.chomp
 puts
-p dates(line)
+p lesser_than(line)

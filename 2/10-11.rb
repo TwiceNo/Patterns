ec8d7f9ea185bd87ea30dbaa316338917ca51651
TwiceNo <ri.twice.no@gmail.com>
2021-03-29 08:40:34 +0300

    task 11 complete

--- 2/10-11.rb
+++ 2/10-11.rb
@@ -16,7 +16,41 @@ def greater_than(line)
   line.scan(/[6-9]/).count
 end
 
+
+def process(param, lines)
+  puts "\nTask #{param}\n"
+  puts show(param)
+  case param
+  when 10
+    puts dates(lines)
+  when 4
+    puts lesser_than(lines)
+  when 11
+    puts unused(lines)
+  when 15
+    puts greater_than(lines)
+  end
+end
+
+def show(param)
+  case param
+  when 10
+    "Dates:"
+  when 4
+    "X < 5"
+  when 11
+    "Unused letters:"
+  when 15
+    "X > 5"
+  else
+    "Unknown task"
+  end
+end
+
+
 puts "Enter line:"
 line = gets.chomp
-puts
-p unused(line)
+
+ARGV.each { |el|
+  process(el.to_i, line)
+}

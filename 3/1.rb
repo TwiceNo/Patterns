1cd6676f53056e9918f86f15c8d69392d35f54f7
TwiceNo <ri.twice.no@gmail.com>
2021-04-04 22:18:49 +0300

    task 4

--- 3/1.rb
+++ 3/1.rb
@@ -8,14 +8,13 @@ class Employee
                 :last_job,
                 :last_speciality,
                 :salary)
-	attr_reader :phone, :email
+	attr_reader :phone, :email, :employee_name
 
 	#--------------------
 	#------Override------
 	#--------------------
 
 	def initialize(name, bdate, phone, address, email, passport, spec, exp, ljob = nil, lspec = nil, sal = nil)
-		@employee_name = name
 		@birthdate     = bdate
 		@address       = address
 		@passport      = passport
@@ -28,6 +27,7 @@ class Employee
 		end
 		self.phone = phone
 		self.email = email
+		self.employee_name = name
 	end
 
 
@@ -51,6 +51,8 @@ class Employee
 			puts "Invalid phone number"
 		when 2
 			puts "Invalid email"
+		when 3
+			puts "Invalid name"
 		end
 	end
 
@@ -60,7 +62,7 @@ class Employee
 
 	def phone= (number)
 		if phone_validation(number)
-			@phone = self.reformat(number)
+			@phone = self.reformat_phone(number)
 		else
 			self.exception(1)
 		end
@@ -74,6 +76,14 @@ class Employee
 		end
 	end
 
+	def employee_name= (name)
+		if name_validation(name)
+			@employee_name = reformat_name(name)
+		else
+			self.exception(3)
+		end
+	end
+
 	#--------------------
 	#-----Validation-----
 	#--------------------
@@ -82,13 +92,36 @@ class Employee
 		phone_number =~ /(8|\+?7)( ?\-?\(?\d{3} ?\)?\-?)(\-? ?(\d{7}|(\d{3})(\-? ?\d{2}){2}))/
 	end
 
-	def reformat(number)
+	def email_validation(email)
+		email =~ /.+@.+\..+/i
+	end
+
+	def name_validation(name)
+		name =~ /(([а-я]+)(( *\-+ *)?([а-я]+)?) *){2} *(([а-я]+)( *([а-я]+)?))/i
+	end
+
+	#--------------------
+	#------Reformat------
+	#--------------------
+
+	def reformat_phone(number)
 		number.chars.reject! {|el| el =~ /[^\d]/}
 		"#{number[0]}-#{number[1..3]}-#{number[4..]}"
 	end
 
-	def email_validation(email)
-		email =~ /.+@.+\..+/i
+	def reformat_name(name)
+		words = name.scan(/([а-я]+)|(\-+)/i).flatten.compact.map { |el| el =~ /\-+/ ? "-" : el.capitalize }
+		indices = words.map.with_index { |el, i| i if el == "-" }.compact
+		indices.each do |index|
+			words[index - 1] = words[index - 1 .. index + 1].join()
+			words[index] = nil
+			words[index + 1] = nil
+		end
+		words.compact!
+		if words.length == 4
+			words[-1].downcase!
+		end
+		words.join(" ")
 	end
 
 end
@@ -107,6 +140,16 @@ class TestEmployee
 		end
 	end
 
+	def add_person(amount)
+		amount.times do
+			@employee_list.append (Employee.new(*enter_person()))
+		end
+	end
+
+	def enter_person
+		Array.new(11) { STDIN.gets.chomp }
+	end
+
 	def show
 		@employee_list.each do |elem|
 			puts elem.to_s
@@ -119,6 +162,6 @@ end
 
 require_relative 'generate_person'
 test = TestEmployee.new
-test.new_person(10)
+test.new_person(1)
 test.show
 

3067500cc5c554783e28aa1af027f4c0517a3291
TwiceNo <ri.twice.no@gmail.com>
2021-04-04 19:01:24 +0300

    task 3

--- 3/1.rb
+++ 3/1.rb
@@ -1,7 +1,6 @@
 class Employee
   attr_accessor(:employee_name,
                 :birthdate,
-                :phone,
                 :address,
                 :email,
                 :passport,
@@ -10,13 +9,15 @@ class Employee
                 :last_job,
                 :last_speciality,
                 :salary)
+	attr_reader :phone
 
-	def initialize(name, bdate, phone, address,
-								 email, passport, spec, exp,
-								 ljob = nil, lspec = nil, sal = nil)
+	#--------------------
+	#------Override------
+	#--------------------
+
+	def initialize(name, bdate, phone, address, email, passport, spec, exp, ljob = nil, lspec = nil, sal = nil)
 		@employee_name = name
 		@birthdate     = bdate
-		@phone         = phone
 		@address       = address
 		@email         = email
 		@passport      = passport
@@ -27,25 +28,57 @@ class Employee
 			@last_speciality = lspec
 			@salary = sal
 		end
+		self.phone = phone
 	end
 
+
 	def to_s
-		string = "ФИО:	             #{@employee_name}
-Дата рождения:       #{@birthdate }
-Телефон:             #{@phone     }
-Адрес:               #{@address   }
-Почта:               #{@email     }
-Паспорт серия номер: #{@passport  }
-Специальность:       #{@speciality}
-Стаж:                #{@experience}
-"
-		if @experience > 0
-			string += "Предыдущее место работы: #{@last_job}
-Должность:   		 #{@last_speciality}
-Зарплата:    		 #{@salary}"
-		end
-		string
+		[@employee_name,
+		 @birthdate,
+		 @phone,
+		 @address,
+		 @email,
+		 @passport,
+		 @speciality,
+		 @experience,
+		 @last_job,
+		 @last_speciality,
+		 @salary].compact.join("\n")
 	end
+
+	def exception(error)
+		case error
+		when 1
+			puts "Invalid phone number"
+		end
+	end
+
+	#--------------------
+	#------Setters-------
+	#--------------------
+
+	def phone= (number)
+		if phone_validation(number)
+			@phone = self.reformat(number)
+		else
+			self.exception(1)
+		end
+	end
+
+
+	#--------------------
+	#-----Validation-----
+	#--------------------
+
+	def phone_validation(phone_number)
+		phone_number =~ /(8|\+?7)( ?\-?\(?\d{3} ?\)?\-?)(\-? ?(\d{7}|(\d{3})(\-? ?\d{2}){2}))/
+	end
+
+	def reformat(number)
+		number.chars.reject! {|el| el =~ /[^\d]/}
+		"#{number[0]}-#{number[1..3]}-#{number[4..]}"
+	end
+
 end
 
 
@@ -70,7 +103,10 @@ class TestEmployee
 	end
 end
 
+
+
 require_relative 'generate_person'
 test = TestEmployee.new
 test.new_person(10)
 test.show
+

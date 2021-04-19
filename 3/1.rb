e4c8e29f6f4433cc8c5fa2faac08d63b5bc15159
TwiceNo <ri.twice.no@gmail.com>
2021-04-19 09:55:02 +0300

    task 11

--- 3/1.rb
+++ 3/1.rb
@@ -200,6 +200,27 @@ class TestEmployee
 end
 
 
+class TerminalViewListEmployee
+	attr_accessor (:employee_list)
+
+	def initialize
+		@employee_list = Array.new()
+	end
+
+	def show
+		@employee_list.each do |elem|
+			puts elem.to_s
+			puts
+		end
+	end
+
+	def add
+		
+	end
+end
+
+
+
 
 require_relative 'generate_person'
 test = TestEmployee.new
